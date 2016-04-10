7253f84a72f307b1b699cad3dddaf76d66fdc6cb
Mike Branstein <mike.branstein@gmail.com>
2016-04-10 16:06:54 -0400

    add notes for future job processing with vsts agent

--- build/gitPull.sh
+++ build/gitPull.sh
@@ -3,3 +3,9 @@ echo Starting Git Pull
 pwd
 git pull https://github.com/mikebranstein/IoT master
 echo Ending Git Pull 
+
+# node agent/vsoagent
+# Ctrl-Z
+# jobs
+# bg 1 (to keep a job running in bg)
+# fg 1 (to move a job to foreground)
