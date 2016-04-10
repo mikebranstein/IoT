fac031300c81c31f5d6022a65d3637fd4e62fa91
Mike Branstein <mike.branstein@gmail.com>
2016-04-09 20:48:59 -0400

    add blink 3 script

---
+++ raspberry-pi/led-blink3.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+def blink(ledPin, onTime, offTime):
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(onTime)
+    GPIO.output(ledPin, GPIO.LOW)
+    time.sleep(offTime)
+    return
+    
+def blinkThree(redLedPin, blueLedPin, greenLedPin):
+    blink(redLedPin, 1, 0)
+    blink(blueLedPin, 1, 0)
+    blink(greenLedPin, 1, 0)
+  
+# tell GPIO to use the Pi's board numbering for pins
+GPIO.setmode(GPIO.BOARD)
+
+redLedPin = 22
+blueLedPin = 14
+greenLedPin = 12
+
+# set data direction of LED pin to output
+GPIO.setup(redLedPin, GPIO.OUT)
+GPIO.setup(blueLedPin, GPIO.OUT)
+GPIO.setup(greenLedPin, GPIO.OUT)
+
+# loop until keyboard interrupt
+
+try:
+    while True:
+        blinkThree(redLedPin, blueLedPin, greenLedPin)
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
