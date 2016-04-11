bcdc1b9508e55e52c9245f12c533b69e262c4543
Mike Branstein <mike.branstein@gmail.com>
2016-04-10 21:10:25 -0400

    add light sensor script

---
+++ raspberry-pi/light-sensor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+ 
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+ 
+import RPi.GPIO as GPIO, time, os      
+ 
+DEBUG = 1
+GPIO.setmode(GPIO.BOARD)
+ 
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.1)
+ 
+        GPIO.setup(RCpin, GPIO.IN)
+        # This takes about 1 millisecond per loop cycle
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+ 
+def loop():
+        print RCtime(12)     # Read RC timing using pin #12
+
+
+#loop until keyboard interrupt
+try:
+    while True:
+        loop()
+except KeyboardInterrupt:
+    pass
+    
+GPIO.cleanup()
