9b586b140147ad48836bec387c02707e5668b2e6
Mike Branstein <mike.branstein@gmail.com>
2016-04-10 21:35:38 -0400

    fixed board layout, remove debugging messages

--- raspberry-pi/light-sensor.py
+++ raspberry-pi/light-sensor.py
@@ -11,20 +11,17 @@ def RCtime(RCpin):
     reading = 0
     GPIO.setup(RCpin, GPIO.OUT)
     GPIO.output(RCpin, GPIO.LOW)
-    print("GPIO.LOW is ", GPIO.LOW)
     time.sleep(0.1)
 
     GPIO.setup(RCpin, GPIO.IN)
     # This takes about 1 millisecond per loop cycle
     while (GPIO.input(RCpin) == GPIO.LOW):
-        print(GPIO.input(RCpin))
         reading += 1
     return reading
  
 #loop until keyboard interrupt
 try:
     while True:
-        print("here")
         # Read RC timing using pin #12
         print(RCtime(12))
 except KeyboardInterrupt:
